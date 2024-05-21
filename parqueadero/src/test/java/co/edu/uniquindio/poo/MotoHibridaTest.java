1eac843924872a0ba6ceb088552d20cb9a85791a
juan-rojas-afk <159515437+juan-rojas-afk@users.noreply.github.com>
2024-05-21 02:24:18 -0500

    MotoHirbridatest

---
+++ parqueadero/src/test/java/co/edu/uniquindio/poo/MotoHibridaTest.java
@@ -0,0 +1,43 @@
+package co.edu.uniquindio.poo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MotoHibridaTest {
+
+    private MotoHibrida moto;
+
+    @BeforeEach
+    public void setUp() {
+        moto = new MotoHibrida("XYZ789", "Honda", "Maria Lopez", 150);
+    }
+
+    @Test
+    public void testGetPlaca() {
+        assertEquals("XYZ789", moto.getPlaca());
+    }
+
+    @Test
+    public void testGetModelo() {
+        assertEquals("Honda", moto.getModelo());
+    }
+
+    @Test
+    public void testGetPropietario() {
+        assertEquals("Maria Lopez", moto.getPropietario());
+    }
+
+    @Test
+    public void testGetVelocidadMaxima() {
+        assertEquals(150, moto.getVelocidadMaxima());
+    }
+
+    @Test
+    public void testGetTipo() {
+        // Supongamos que Parqueadero.TIPO_MOTO_CLASICA es una constante que tiene el valor 2
+        assertEquals(2, moto.getTipo());
+    }
+}
+
+    
